5b8610b44c3212d5fa7a6c1514b358d901314e6d
Syed Salman <37833818+alphamod@users.noreply.github.com>
2020-11-10 03:08:32 +0530

    environment variables added

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: `https://www.googleapis.com/youtube/v3`,
     params: {
-        key: `AIzaSyBUdVzC-PW0qCyyWF9k95g8Dpr6piECHjQ`
+        key: process.env.REACT_APP_API_KEY
     }
 });
